5105fe97e76924bec95e125f060a95e81a4d8f7b
Sam Pagenkopf <ssaammp@gmail.com>
2024-04-16 19:47:53 +0000

    add optimization to WASM compile

--- emulator/scripts/dev.sh
+++ emulator/scripts/dev.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-export CFLAGS="-g"
+export CFLAGS="-g -Og"
 export WASM_CMD="emmake make wasm"
 $WASM_CMD
-simple-file-watch --path="src" --command="$WASM_CMD"
+if [[ -v WASM_WATCH ]]; then
+	echo "Running watch server"
+	simple-file-watch --path="src" --command="$WASM_CMD"
+fi
