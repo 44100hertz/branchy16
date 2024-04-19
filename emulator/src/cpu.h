48ddc489c87ed1d4758f32239a897c3d4b8cb80e
Sam Pagenkopf <ssaammp@gmail.com>
2024-04-19 19:20:00 +0000

    silence warnings about "bool" for older compilers

--- emulator/src/cpu.h
+++ emulator/src/cpu.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <stdint.h>
+#pragma once
+#include <stdbool.h>
 
 typedef uint8_t byte;
 typedef uint16_t word;
--- emulator/src/tests.c
+++ emulator/src/tests.c
@@ -1,5 +1,6 @@
 #include "tests.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
